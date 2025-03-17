5976ef432b8cfa16011581e66a8e0f373d49962c
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-03-17 23:32:00 +0000

    migrations create-table-role and create-table-user

---
+++ src/main/resources/db/migration/V0001__create-table-role.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tbl_role(
+    role_id SERIAL PRIMARY KEY,
+    role_name varchar(255)
+);
---
+++ src/main/resources/db/migration/V0002__create-table-user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS tbl_user(
+    user_id BIGSERIAL primary key,
+    username varchar(255) NOT NULL UNIQUE,
+    "password" varchar(255) NOT NULL,
+    role_id  bigint,
+    CONSTRAINT fk_userid_roleid
+        FOREIGN KEY (role_id)
+            REFERENCES tbl_role(role_id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+);
